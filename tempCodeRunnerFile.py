2e0da4d4b3dc9fc9cec5d477a2e2af02eff74c4a
Dhruv Agrawal <104168747+dhruv1210@users.noreply.github.com>
2024-08-22 02:04:17 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+ elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
